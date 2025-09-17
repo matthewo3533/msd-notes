acb1565fde08e10fcc76d17d8f6ed0a04302bb66
matthewo3533 <54376649+matthewo3533@users.noreply.github.com>
2025-09-17 23:45:19 +1200

    dfg

--- src/components/FurniturePage.tsx
+++ src/components/FurniturePage.tsx
@@ -18,6 +18,7 @@ const FurniturePage: React.FC = () => {
     recoveryRate: 0,
     directCredit: '',
     paymentReference: '',
+    paymentCardNumber: '',
     income: {
       benefit: 0,
       employment: 0,
@@ -48,6 +49,7 @@ const FurniturePage: React.FC = () => {
       recoveryRate: 0,
       directCredit: '',
       paymentReference: '',
+    paymentCardNumber: '',
       income: {
         benefit: 0,
         employment: 0,
